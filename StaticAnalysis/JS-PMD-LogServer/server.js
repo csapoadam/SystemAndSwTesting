dee5f465156fcc2d6845f3ccc6b8bc570171dd47
csapoadam <csapo.adam@gmail.com>
2022-03-01 15:33:15 +0100

    changes based on pmd output

--- StaticAnalysis/JS-PMD-LogServer/server.js
+++ StaticAnalysis/JS-PMD-LogServer/server.js
@@ -3,10 +3,8 @@ const fs = require('fs')
 const net = require('net')
 const os = require('os')
   
-var SERVER;
-  
 const setupServer = (dataHandlerFun, closeHandler) => {
-  SERVER = net.createServer();
+  const SERVER = net.createServer();
 
   const handleConnection = (conn) => {
     
@@ -36,6 +34,39 @@ const setupServer = (dataHandlerFun, closeHandler) => {
 const args = process.argv;
 const OUTPUT_FILE = args[2];
 
+let BufferManager = (function() {
+  const buf_size = 3000
+  let cur_inx = 0
+  const data_buf = new Array(3000).fill('');
+  return {
+    addElement: (elem) => {
+      data_buf[cur_inx] = elem
+      cur_inx++
+
+      if (cur_inx == buf_size) {
+        let dte = new Date(Date.now());
+        let str = data_buf.join(os.EOL) + '-' + dte.toString() + os.EOL;
+    
+        fs.appendFile(OUTPUT_FILE, str, (err) => {
+          if (err) { console.error(err); }
+        })
+        cur_inx = 0;
+      }
+    },
+    flush: () => {
+      if (cur_inx > 0) {
+        let dte = new Date(Date.now()); 
+        let str = data_buf.join(os.EOL) + '-' + dte.toString() + os.EOL;
+        fs.appendFile(OUTPUT_FILE, str, (err) => {
+          if (err) { console.error(err); }
+        })
+      }
+    }
+  }
+})()
+
+console.log(data_buf)
+
 const BUF_SIZE = 3000
 let DATA_BUFFER = new Array(BUF_SIZE).fill('');
 let CUR_INX = 0;
@@ -44,28 +75,12 @@ const onReceiveData = (buf) => {
   let strRcvd = buf.toString('utf-8')
   let pushHandleL = parseInt(strRcvd.slice(43,48).replace(' ', ''), 16)
   let pushHandleR = parseInt(strRcvd.slice(37,42).replace(' ', ''), 16)
-  let startButton = parseInt(strRcvd.slice(14,15), 16) == 4
-  DATA_BUFFER[CUR_INX] = `${pushHandleL}-${pushHandleR}-${startButton}`
-  CUR_INX++
-  if (CUR_INX == BUF_SIZE) {
-    let dte = new Date(Date.now());
-    let str = DATA_BUFFER.join(os.EOL) + '-' + dte.toString() + os.EOL;
-    
-    fs.appendFile(OUTPUT_FILE, str, (err) => {
-      if (err) console.error(err);
-    })
-    CUR_INX = 0;
-  }
+  let startButton = parseInt(strRcvd.slice(14,15), 16) === 4
+  BufferManager.addElement(`${pushHandleL}-${pushHandleR}-${startButton}`)
 }
 
 const onCloseHandler = () => {
-  if (CUR_INX > 0) {
-    let dte = new Date(Date.now()); 
-    let str = DATA_BUFFER.join(os.EOL) + '-' + dte.toString() + os.EOL;
-    fs.appendFile(OUTPUT_FILE, str, (err) => {
-      if (err) console.error(err);
-    })
-  }
+  BufferManager.flush()
 }
 
 setupServer(onReceiveData, onCloseHandler);
